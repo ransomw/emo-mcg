2451337bb27a1080be07f5f3cfa3bb703e2911eb
ransom <auvergnerw@gmail.com>
2017-07-24 03:22:57 -0700

    wip -- init-exp-test  lein test emcg.db-test passes

--- exp/test/clj/emcg/db_test.clj
+++ exp/test/clj/emcg/db_test.clj
@@ -24,13 +24,36 @@
              (set (map :seq-num exp-defn))))
       (is (= (count exp-defn)
              (count (set (map :emo-id exp-defn)))))
+      (let [mcg-ids
+            (map :mcg-id (flatten (map :mcg-trials exp-defn)))]
+        (is (= (count mcg-ids) (count (set mcg-ids)))))
       (->>
        exp-defn
        (map
-        (fn [emo-block]
-          (is (contains? (set (range (:num-emo exp-stim-config)))
-                         (:emo-idx emo-block)))
-          
+        (fn [{:keys [emo-id emo-idx seq-num mcg-trials]}]
+          (is (integer? emo-id))
+          (is (integer? emo-idx))
+          (is (integer? seq-num))
+          (is (seq? mcg-trials))
+          (is (contains?
+               (set (range (:num-emo exp-stim-config)))
+               emo-idx))
+          (is (= (set (range (count mcg-trials)))
+                 (set (map :seq-num mcg-trials))))
+          (->>
+           mcg-trials
+           (map
+            (fn [{:keys [mcg-id seq-num idx-v idx-a]}]
+              (is (integer? mcg-id))
+              (is (integer? seq-num))
+              (is (contains?
+                   (set (range (:num-mcg exp-stim-config)))
+                   idx-v))
+              (is (contains?
+                   (set (range (:num-mcg exp-stim-config)))
+                   idx-a))
+              ))
+           doall)
           ))
        doall)
     )))
