fd97372cde0d0799a38c28c0ea079dc896d19e0d
ransom <auvergnerw@gmail.com>
2017-07-25 03:48:35 -0700

    wip -- init /exp endpoint test

--- exp/test/clj/emcg/routes_test.clj
+++ exp/test/clj/emcg/routes_test.clj
@@ -4,6 +4,7 @@
    [ring.middleware.format :refer [wrap-restful-format]]
 
    [clojure.test :refer :all]
+   [clojure.edn :as edn]
    [ring.mock.request :as mock]
 
    [emcg.server :as s]
@@ -14,8 +15,7 @@
 
 (def test-handler
   (->
-   ;; (r/routes-expone)
-   r/routes-main
+   (r/routes-expone)
    (wrap-defaults api-defaults)
    (wrap-restful-format :format [:edn])
    ))
@@ -30,11 +30,35 @@
 
 (deftest init-exp
   (let [res
-        (
-         ;; test-handler
-         s/http-handler
-         (mock/request :get
-                       "/"
-                       ))]
+        (test-handler
+         (mock/request :post "/exp"))
+        body (edn/read-string (:body res))]
     (is (not (nil? res)))
-    ))
+    (is (map? res))
+    (is (= 200 (:status res)))
+    (is (map body))
+    (let [{exp-id :id exp-defn :defn} body]
+      (is (integer? exp-id))
+      (is (list? exp-defn))
+      (->>
+       exp-defn
+       (map
+        (fn [{:keys [emo-id mcg-ids av-idxs]}]
+          (is (integer? emo-id))
+          (is (= (set '(true)) (set (map integer? mcg-ids))))
+          (is (= (set '(true)) (set (map integer? (flatten av-idxs)))))
+          (is (= (set '(2)) (set (map count av-idxs))))
+          (is (= (count mcg-ids) (count (set mcg-ids))))
+          (is (= (set '(true))
+                 (set (->>
+                       (flatten av-idxs)
+                       (map
+                        #(contains?
+                          (set (range (:num-mcg exp-stim-config))) %))
+                       doall))))
+          ))
+       doall)
+      (is (= (count exp-defn) (count (set (map :emo-id exp-defn)))))
+      (is (= 1 (count (set (map count (map :mcg-ids body))))))
+      (is (= 1 (count (set (map count (map :av-idxs body))))))
+      )))
